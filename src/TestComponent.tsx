16e517258e57edaa62896c8b4d71c750585e235a
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-20 15:27:32 +0900

    useState

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,16 +1,34 @@
 // rafceの変換を使うとテンプレートが使える
-import React from 'react'
+// useStateを使ってファンクショナルコンポーネントの中で変更する
+import React, { useState } from 'react'
+
 
 // propsの定義
 interface Props{
   text: string;
 }
 
+// オブジェクト型で作りたい時に
+interface UserData{
+  id: number;
+  name: string;
+}
+
 // Propsの型を指定 引数に変数を指定する
 const TestComponent: React.FC<Props> = (props) => {
+  // コンポーネントないで自由に使える変数を作成 useStateは初期値を入れている
+  // nullを入れたい場合
+  const [count, setCount] = useState<number | null>(0);
+  // オブジェクト型useStateを使う際は<>は作成したインターフェースを指定する
+  const [user, setUser] = useState<UserData>({
+    id: 1,
+    name: "name"
+  });
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1> {count} </h1>
     </div>
   )
 }
